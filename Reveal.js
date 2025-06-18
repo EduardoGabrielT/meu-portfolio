dcda1030533f01737efc4eb38f912a3affa54e2c
EDUARDO GABRIEL <programador.edu.dev@gmail.com>
2025-06-18 18:12:10 -0300

    Primeiro commit: adicionando arquivos iniciais do portfólio

---
+++ Reveal.js
@@ -0,0 +1,115 @@
+window.revelar = ScrollReveal({
+  reset:true
+})
+//top do site//
+
+revelar.reveal('.efeito-txt-site',
+{
+  duration: 2000,
+  distance: '90px'
+})
+
+revelar.reveal('.efeito-img-top',
+{
+  duration: 2000,
+  distance: '90px', 
+  daley: 500
+})
+//titulo das sessoes//
+revelar.reveal('.titulo',
+{
+  duration: 2000,
+  distance: '90px'
+})
+//sessao especialidades//
+revelar.reveal('.efeito-especialidades1',
+{
+  duration: 2000,
+  distance: '90px',
+  delay: 500
+})
+revelar.reveal('.efeito-especialidades2',
+{
+  duration: 2000,
+  distance: '90px',
+  delay: 1000
+})
+revelar.reveal('.efeito-especialidades3',
+{
+  duration: 2000,
+  distance: '90px',
+  delay: 1500
+})
+//sessao sobre//
+revelar.reveal('.efeito-img-sobre',
+{
+  duration: 2000,
+  distance: '90px',
+  delay: 500,
+  origin:'left'
+})
+revelar.reveal('.efeito-txt-sobre',
+{
+  duration: 2000,
+  distance: '90px',
+  delay: 1000,
+  origin:'right'
+})
+//sessao portifolio//
+revelar.reveal('.efeito-portifolio1',
+{
+  duration: 2000,
+  distance: '90px',
+  delay: 500,
+  origin:'botton'
+})
+revelar.reveal('.efeito-portifolio2',
+{
+  duration: 1000,
+  distance: '90px',
+  delay: 1000,
+  origin:'botton'
+})
+revelar.reveal('.efeito-portifolio3',
+{
+  duration: 1200,
+  distance: '90px',
+  delay: 1200,
+  origin:'botton'
+})
+//sessao formulario//
+revelar.reveal('.efeito-form',
+{
+  duration: 500,
+  distance: '90px',
+  delay: 500,
+  origin:'top'
+})
+revelar.reveal(' .efeito-form1',
+{
+  duration: 1000,
+  distance: '90px',
+  delay: 1000,
+  origin:'top'
+})
+revelar.reveal(' .efeito-form2',
+{
+  duration: 1200,
+  distance: '90px',
+  delay: 1200,
+  origin:'top'
+})
+revelar.reveal(' .efeito-form3',
+{
+  duration: 1300,
+  distance: '90px',
+  delay: 1300,
+  origin:'top'
+})
+revelar.reveal(' .efeito-form4',
+{
+  duration: 1300,
+  distance: '90px',
+  delay: 1300,
+  origin:'top'
+})
